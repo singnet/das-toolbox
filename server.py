cda45ff057b4e1e3c5f2566b8026bb1b849baa34
levisingularity <rafael.levi@singularitynet.io>
2024-01-29 15:36:03 -0300

    Initiate Redis and MongoDB Servers
    * Executing the 'server start' command will launch both the Redis and MongoDB servers."

--- server.py
+++ server.py
@@ -13,8 +13,23 @@ def server():
 @click.option("--redis-port", help="", required=False, default=6379)
 @click.option("--mongodb-port", help="", required=False, default=27017)
 def start(redis_port, mongodb_port):
-    docker_client.containers.run("redis:7.2.3-alpine", detach=True)
-    docker_client.containers.run("mongo:6.0.13-jammy", detach=True)
+    redis_container = docker_client.containers.run(
+        "redis:7.2.3-alpine",
+        detach=True,
+        ports={
+            "6379/tcp": redis_port,
+        },
+    )
+    mongodb_container = docker_client.containers.run(
+        "mongo:6.0.13-jammy",
+        detach=True,
+        ports={
+            "27017/tcp": mongodb_port,
+        },
+    )
+
+    print(redis_container)
+    print(mongodb_container)
 
 
 @server.command()
@@ -22,4 +37,5 @@ def stop():
     containers = docker_client.containers.list()
 
     for container in containers:
+        print(f"Parando container: {container.id}")
         container.stop()
