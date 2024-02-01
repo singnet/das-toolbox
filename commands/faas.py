0451df136c0f18a2e56e5accb82e86230510a18e
levisingularity <rafael.levi@singularitynet.io>
2024-01-31 21:05:25 -0300

    Introduced the 'faas' command subgroup.
    * Added the 'faas start' command to initiate the specified OpenFaaS function.
    * Implemented the functionality to save running containers initiated through the CLI to the ~/.das/containers.json file.
    * Incorporated the 'faas stop' command to halt containers started by the 'start' command.

---
+++ commands/faas.py
@@ -0,0 +1,77 @@
+import click
+from services.container import ContainerService
+from services.config import ConfigService
+from exceptions import ContainerAlreadyRunningException, ContainerNotRunningException
+from enum import Enum
+
+
+class FunctionEnum(Enum):
+    QUERY_ENGINE = "queryengine"
+    ATOM_DB = "atomdb"
+
+
+@click.group()
+def faas():
+    global container_service
+    global config_service
+
+    config_service = ConfigService()
+    container_service = ContainerService()
+
+    config_service.load()
+    if not config_service.is_config_ready():
+        click.echo(
+            "Configuration is not ready. Please initialize the configuration first."
+        )
+        exit(1)
+
+
+@faas.command()
+@click.option(
+    "--function",
+    help="",
+    required=True,
+    type=click.Choice([e.value for e in FunctionEnum]),
+)
+@click.option(
+    "--version",
+    help="",
+    required=True,
+    type=str,
+)
+def start(function, version):
+    redis_port = config_service.get("redis.port")
+    mongodb_port = config_service.get("mongodb.port")
+    mongodb_username = config_service.get("mongodb.username")
+    mongodb_password = config_service.get("mongodb.password")
+    external_port = 8080
+    repository = "trueagi/das"
+
+    try:
+        click.echo("Starting FaaS...")
+
+        container_service.setup_openfaas(
+            repository,
+            function,
+            version,
+            external_port,
+            redis_port,
+            mongodb_port,
+            mongodb_username,
+            mongodb_password,
+        )
+
+        click.echo(f"FaaS running on port {external_port}")
+    except ContainerAlreadyRunningException:
+        click.echo("The FaaS container is already running")
+        exit(1)
+    except ContainerNotRunningException:
+        click.echo(
+            "Redis or MongoDB is not running. Please use 'server start' to start the required services before running 'faas start'."
+        )
+        exit(1)
+
+
+@faas.command()
+def stop():
+    container_service.prune()
--- services/container.py
+++ services/container.py
@@ -1,29 +1,136 @@
-import docker
 import os
+import json
+import docker
+from exceptions import ContainerAlreadyRunningException, ContainerNotRunningException
+from docker.errors import NotFound as ContainerNotFound
+
+DEFAULT_CONTAINERS_PATH = os.path.expanduser("~/.das/containers.json")
+
+
+class Container:
+    def __init__(self, name, image=None, image_version="latest") -> None:
+        self._name = name
+        self._image = image
+        self._image_version = image_version
+
+    def get_name(self) -> str:
+        return self._name
+
+    def get_image(self) -> str:
+        return f"{self._image}:{self._image_version}"
+
+    def set_image(self, image: str) -> None:
+        self._image = image
+
+    def set_image_version(self, image_version: str) -> None:
+        self._image_version = image_version
+
+    def append_to_running_file(self, id: str, filename=DEFAULT_CONTAINERS_PATH):
+        container_data = {
+            "name": self._name,
+            "id": id,
+            "image": self.get_image(),
+        }
+
+        if os.path.exists(filename):
+            with open(filename, "r") as json_file:
+                data = json.load(json_file)
+                if "running" in data:
+                    data["running"].append(container_data)
+                else:
+                    data["running"] = [container_data]
+        else:
+            data = {"running": [container_data]}
+
+        with open(filename, "w") as json_file:
+            json.dump(data, json_file, indent=2)
+
+    @staticmethod
+    def remove_all_running(filename=DEFAULT_CONTAINERS_PATH):
+        if os.path.exists(filename):
+            with open(filename, "r") as json_file:
+                data = json.load(json_file)
+                if "running" in data:
+                    data["running"] = []
+
+            with open(filename, "w") as json_file:
+                json.dump(data, json_file, indent=2)
 
 
 class ContainerService:
     def __init__(self) -> None:
         self.docker_client = docker.from_env()
-        self.redis_image_name = "redis:7.2.3-alpine"
-        self.mongodb_image_name = "mongo:6.0.13-jammy"
-        self.canonical_load = "levisingnet/canonical-load:latest"
-
-    def is_redis_running(self):
-        containers = self.docker_client.containers.list(
-            filters={"ancestor": self.redis_image_name}
+        self.redis = Container("das-redis", "redis", "7.2.3-alpine")
+        self.mongodb = Container("das-mongodb", "mongo", "6.0.13-jammy")
+        self.canonical_load = Container(
+            "das-canonical-load",
+            "levisingnet/canonical-load",
         )
+        self.openfaas = Container("das-openfaas")
+
+    def is_container_running(self, image: str) -> bool:
+        containers = self.docker_client.containers.list(filters={"ancestor": image})
         return len(containers) > 0
 
-    def is_mongodb_running(self):
-        containers = self.docker_client.containers.list(
-            filters={"ancestor": self.mongodb_image_name}
-        )
-        return len(containers) > 0
+    def setup_openfaas(
+        self,
+        repository: str,
+        function: str,
+        version: str,
+        external_port: int,
+        redis_port: int,
+        mongodb_port: int,
+        mongodb_username: str,
+        mongodb_password: str,
+        internal_port: int = 8080,
+    ) -> str:
+        function_full_version = f"v{version}-{function}"
+        function_image = f"{repository}:{function_full_version}"
 
-    def setup_redis(self, redis_port: int) -> None:
-        redis_container = self.docker_client.containers.run(
-            self.redis_image_name,
+        if not self.is_container_running(
+            self.redis.get_image()
+        ) or not self.is_container_running(self.mongodb.get_image()):
+            raise ContainerNotRunningException()
+
+        if self.is_container_running(function_image):
+            raise ContainerAlreadyRunningException(
+                f"The FaaS container is alreday running"
+            )
+
+        self.openfaas.set_image(function_image)
+        self.openfaas.set_image_version(function_full_version)
+
+        docker_response = self.docker_client.containers.run(
+            function_image,
+            name=self.openfaas.get_name(),
+            detach=True,
+            network_mode="host",
+            restart_policy={
+                "Name": "on-failure",
+                "MaximumRetryCount": 5,
+            },
+            environment={
+                "DAS_MONGODB_HOSTNAME": "localhost",
+                "DAS_REDIS_HOSTNAME": "localhost",
+                "DAS_MONGODB_NAME": "das",
+                "DAS_MONGODB_PASSWORD": mongodb_password,
+                "DAS_MONGODB_PORT": mongodb_port,
+                "DAS_MONGODB_USERNAME": mongodb_username,
+                "DAS_REDIS_PORT": redis_port,
+            },
+        )
+
+        self.openfaas.append_to_running_file(docker_response.id)
+
+        return docker_response.id
+
+    def setup_redis(self, redis_port: int) -> str:
+        if self.is_container_running(self.redis.get_image()):
+            raise ContainerAlreadyRunningException()
+
+        docker_response = self.docker_client.containers.run(
+            self.redis.get_image(),
+            name=self.redis.get_name(),
             detach=True,
             restart_policy={
                 "Name": "on-failure",
@@ -34,16 +141,22 @@ class ContainerService:
             },
         )
 
-        print(redis_container.id)
+        self.redis.append_to_running_file(docker_response.id)
+
+        return docker_response.id
 
     def setup_mongodb(
         self,
         mongodb_port: int,
         mongodb_username: str,
         mongodb_password: str,
-    ) -> None:
-        mongodb_container = self.docker_client.containers.run(
-            self.mongodb_image_name,
+    ) -> str:
+        if self.is_container_running(self.mongodb.get_image()):
+            raise ContainerAlreadyRunningException()
+
+        docker_response = self.docker_client.containers.run(
+            self.mongodb.get_image(),
+            name=self.mongodb.get_name(),
             detach=True,
             restart_policy={
                 "Name": "on-failure",
@@ -58,7 +171,9 @@ class ContainerService:
             },
         )
 
-        print(mongodb_container.id)
+        self.mongodb.append_to_running_file(docker_response.id)
+
+        return docker_response.id
 
     def setup_canonical_load(
         self,
@@ -68,18 +183,21 @@ class ContainerService:
         mongodb_username,
         mongodb_password,
         redis_port,
-    ) -> None:
+    ) -> str:
         canonical = "--canonical" if canonical_flag else ""
         command = (
             f"python3 scripts/load_das.py {canonical} --knowledge-base {metta_path}"
         )
 
-        if not os.path.exists(metta_path):
-            raise FileNotFoundError(
-                f"The specified path '{metta_path}' does not exist."
-            )
+        if not self.is_container_running(
+            self.redis.get_image()
+        ) or not self.is_container_running(self.mongodb.get_image()):
+            raise ContainerNotRunningException()
 
-        canonical_load_container = self.docker_client.containers.run(
+        if not os.path.exists(metta_path):
+            raise FileNotFoundError()
+
+        docker_response = self.docker_client.containers.run(
             self.canonical_load,
             detach=True,
             command=command,
@@ -107,11 +225,46 @@ class ContainerService:
             },
         )
 
-        print(canonical_load_container.id)
+        self.canonical_load.append_to_running_file(docker_response.id)
+
+        return docker_response.id
+
+    # def stop(self, id) -> None:
+    #     containers = [
+    #         self.redis,
+    #         self.canonical_load,
+    #         self.mongodb,
+    #         self.openfaas,
+    #     ]
+
+    #     for container in containers:
+    #         try:
+    #             if container.id == id:
+    #                 docker_container = self.docker_client.containers.get(container.id)
+
+    #                 docker_container.stop()
+    #                 return
+    #         except ContainerNotFound:
+    #             pass
 
     def prune(self) -> None:
+        # containers = [
+        #     self.redis,
+        #     self.canonical_load,
+        #     self.mongodb,
+        #     self.openfaas,
+        # ]
+
+        # for container in containers:
+        #     try:
+        #         docker_container = self.docker_client.containers.get(container.id)
+
+        #         docker_container.stop()
+        #     except ContainerNotFound:
+        #         pass
         containers = self.docker_client.containers.list()
 
         for container in containers:
-            print(f"Parando container: {container.id}")
             container.stop()
+
+        Container.remove_all_running()
