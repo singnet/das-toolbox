8820a9302f370d3d3c01c32b5e2554881da4767d
levisingularity <rafael.levi@singularitynet.io>
2024-06-27 15:29:02 -0300

    Chores: create a new release version 0.2.20

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 from common.utils import get_server_username
 
-VERSION = '0.2.19'
+VERSION = '0.2.20'
 RELEASE_NOTES_URL = (
     "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 )
