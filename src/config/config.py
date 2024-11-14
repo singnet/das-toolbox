adc3251404886d91208c6460978ffaaf2fa95ef2
levisingularity <>
2024-11-14 08:37:54 -0300

    Chores: create a new release version 0.4.6

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 from common.utils import get_server_username
 
-VERSION = '0.4.5'
+VERSION = '0.4.6'
 RELEASE_NOTES_URL = "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 
 # PATHS
