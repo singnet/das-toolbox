c72b140a33f36863d55bed7d1ac65bfe296e0681
levisingularity <>
2024-11-19 11:52:16 -0300

    Chores: create a new release version 0.4.7

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 from common.utils import get_server_username
 
-VERSION = '0.4.6'
+VERSION = '0.4.7'
 RELEASE_NOTES_URL = "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 
 # PATHS
@@ -23,7 +23,7 @@ MONGODB_IMAGE_NAME = "mongo"
 MONGODB_IMAGE_VERSION = "6.0.13-jammy"
 
 METTA_PARSER_IMAGE_NAME = "trueagi/das"
-METTA_PARSER_IMAGE_VERSION = "0.5.3-metta-parser"
+METTA_PARSER_IMAGE_VERSION = "0.5.4-metta-parser"
 
 OPENFAAS_IMAGE_NAME = "trueagi/openfaas"
 
