456fda52398abcc8d2ce73b3ffeb0d8675cf8152
levisingularity <rafael.levi@singularitynet.io>
2025-03-25 13:39:25 -0300

    das-323: Add inference agent commands (stop, start and restart)

---
+++ das-cli/src/commands/inference_agent/inference_agent_module.py
@@ -0,0 +1,31 @@
+from common import Module
+
+from .inference_agent_cli import (
+    InferenceAgentCli,
+    InferenceAgentContainerManager,
+    Settings,
+)
+
+
+class InferenceAgentModule(Module):
+    _instance = InferenceAgentCli
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._settings = Settings()
+
+        self._dependecy_injection = [
+            (
+                InferenceAgentContainerManager,
+                self._inference_agent_container_manager_factory,
+            ),
+        ]
+
+    def _inference_agent_container_manager_factory(self) -> InferenceAgentContainerManager:
+        container_name = self._settings.get("inference_agent.container_name")
+
+        return InferenceAgentContainerManager(
+            container_name,
+            options={},
+        )
