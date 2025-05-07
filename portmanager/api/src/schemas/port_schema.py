5eea56cf67ef7caf7f20d9c7ed7093bd2b597d4a
levisingularity <rafael.levi@singularitynet.io>
2025-05-07 18:46:05 -0300

    das-238: Add observer schema

--- portmanager/api/src/schemas/port_schema.py
+++ portmanager/api/src/schemas/port_schema.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class PortSchema(Schema):
@@ -39,3 +39,8 @@ class PortWithBindingInstanceSchema(Schema):
     id = fields.Int()
     port_number = fields.Int()
     binding = fields.Nested(PortBindingWithInstanceSchema, allow_none=True)
+
+
+class ObserverRequestSchema(Schema):
+    instance_id = fields.Int(required=True)
+    ports = fields.List(fields.Int(), required=True, validate=validate.Length(min=1))
