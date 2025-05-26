39634812ff4fb1a5ad2e67833dff0e2114ef15f0
levisingularity <rafael.levi@singularity.io>
2025-05-26 19:18:21 -0300

    das-238: Update config.py to support new environment variables

--- gatekeeper/api/src/config.py
+++ gatekeeper/api/src/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///ports.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    API_TOKEN = os.getenv("API_TOKEN", "supersecrettoken")
+    JWT_SECRET = os.getenv("JWT_SECRET", "supersecretjwtkey")
+    JWT_ALGORITHM = "HS256"
     PORT_RANGE_START = int(os.getenv("PORT_RANGE_START", 8000))
     PORT_RANGE_END = int(os.getenv("PORT_RANGE_END", 10000))
