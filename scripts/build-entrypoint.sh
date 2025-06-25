1fab37d3f7a834eca4f3393721d4d12efeb219c2
rafael.levi <rafael.levi@singularitynet.io>
2025-06-25 14:33:21 -0300

    das-toolbox-192: Change library build

--- scripts/build-entrypoint.sh
+++ scripts/build-entrypoint.sh
@@ -4,13 +4,14 @@ set -e
 USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 
-DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-
-groupadd -g "$DOCKER_GID" docker || true
-
 groupadd -g "$GROUP_ID" dasgroup || true
 useradd -m -u "$USER_ID" -g "$GROUP_ID" -G docker -s /bin/bash dasuser || true
 
 chown -R "$USER_ID:$GROUP_ID" /app
+
+git config --global http.version HTTP/1.1
+git config --global http.postBuffer 157286400
+git clone --depth 1 --branch master https://github.com/singnet/das.git /tmp/das || true
+cd /tmp/das && make build-all
 
 exec gosu dasuser make build-local
