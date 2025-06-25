c7f5f93129c0b6b6d0c87d227b5e7341c3b66f9d
rafael.levi <rafael.levi@singularitynet.io>
2025-06-25 13:40:24 -0300

    das-toolbox-192: Build hyperon_das.so

--- scripts/build-entrypoint.sh
+++ scripts/build-entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 
+DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+
+groupadd -g "$DOCKER_GID" docker || true
+
 groupadd -g "$GROUP_ID" dasgroup || true
-useradd -m -u "$USER_ID" -g "$GROUP_ID" -s /bin/bash dasuser || true
+useradd -m -u "$USER_ID" -g "$GROUP_ID" -G docker -s /bin/bash dasuser || true
 
 chown -R "$USER_ID:$GROUP_ID" /app
 
