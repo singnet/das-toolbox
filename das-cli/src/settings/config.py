c1b4c39d2a30955e5f0b5559a2c0365bdb2899e0
levisingularity <rafael.levi@singularitynet.io>
2025-07-15 09:09:38 -0300

    Chores: create a new release version 0.4.13

--- das-cli/src/settings/config.py
+++ das-cli/src/settings/config.py
@@ -1,6 +1,6 @@
 import getpass
 
-VERSION = '0.4.12'
+VERSION = '0.4.13'
 RELEASE_NOTES_URL = "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 
 # PATHS
@@ -22,7 +22,7 @@ MONGODB_IMAGE_NAME = "mongo"
 MONGODB_IMAGE_VERSION = "6.0.13-jammy"
 
 METTA_PARSER_IMAGE_NAME = "trueagi/das"
-METTA_PARSER_IMAGE_VERSION = "0.5.5-metta-parser"
+METTA_PARSER_IMAGE_VERSION = "0.5.6-metta-parser"
 
 OPENFAAS_IMAGE_NAME = "trueagi/openfaas"
 
