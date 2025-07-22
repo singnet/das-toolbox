4b1a0235ef941cb3ed73d650481243f04704f9a1
rafael.levi <rafael.levi@singularitynet.io>
2025-07-22 13:58:27 -0300

    das-toolbox-181: Refactor jupyter notebook commands to enable json and yaml output

--- das-cli/src/commands/jupyter_notebook/jupyter_notebook_cli.py
+++ das-cli/src/commands/jupyter_notebook/jupyter_notebook_cli.py
@@ -1,6 +1,13 @@
 from injector import inject
 
-from common import Command, CommandGroup, CommandOption, Settings, StdoutSeverity
+from common import (
+    Command,
+    CommandGroup,
+    CommandOption,
+    Settings,
+    StdoutSeverity,
+    StdoutType,
+)
 from common.docker.exceptions import (
     DockerContainerDuplicateError,
     DockerContainerNotFoundError,
@@ -9,6 +16,9 @@ from common.docker.exceptions import (
 from common.prompt_types import AbsolutePath
 
 from .jupyter_notebook_container_manager import JupyterNotebookContainerManager
+from .jupyter_notebook_agent_container_service_response import (
+    JupyterNotebookContainerServiceResponse,
+)
 
 
 class JupyterNotebookStart(Command):
@@ -75,31 +85,61 @@ EXAMPLES
         self._settings = settings
         self._jupyter_notebook_container_manager = jupyter_notebook_container_manager
 
+    def _get_container(self):
+        return self._jupyter_notebook_container_manager.get_container()
+
     def run(self, working_dir: str | None = None):
         self._settings.raise_on_missing_file()
         self._settings.raise_on_schema_mismatch()
 
         self.stdout("Starting Jupyter Notebook...")
 
-        jupyter_notebook_port = self._settings.get("services.jupyter_notebook.port")
+        container = self._get_container()
 
         try:
-            self._jupyter_notebook_container_manager.start_container(
-                jupyter_notebook_port,
-                working_dir,
-            )
+            self._jupyter_notebook_container_manager.start_container(working_dir)
+            success_message = f"Jupyter Notebook started on port {container.port}"
             self.stdout(
-                f"Jupyter Notebook started on port {jupyter_notebook_port}",
+                success_message,
                 severity=StdoutSeverity.SUCCESS,
             )
-        except DockerContainerDuplicateError:
             self.stdout(
-                f"Jupyter Notebook is already running. It's listening on port {jupyter_notebook_port}",
+                dict(
+                    JupyterNotebookContainerServiceResponse(
+                        action="start",
+                        status="success",
+                        message=success_message,
+                        container=container,
+                        extra_details={
+                            "working_dir": working_dir,
+                        },
+                    ),
+                ),
+                stdout_type=StdoutType.MACHINE_READABLE,
+            )
+        except DockerContainerDuplicateError:
+            warning_message = f"Jupyter Notebook is already running. It's listening on port {container.port}"
+            self.stdout(
+                warning_message,
                 severity=StdoutSeverity.WARNING,
+            )
+            self.stdout(
+                dict(
+                    JupyterNotebookContainerServiceResponse(
+                        action="start",
+                        status="already_running",
+                        message=warning_message,
+                        container=container,
+                        extra_details={
+                            "working_dir": working_dir,
+                        },
+                    ),
+                ),
+                stdout_type=StdoutType.MACHINE_READABLE,
             )
         except DockerError:
             raise DockerError(
-                f"\nError occurred while trying to start Jupyter Notebook on port {jupyter_notebook_port}\n"
+                f"\nError occurred while trying to start Jupyter Notebook on port {container.port}\n"
             )
 
 
@@ -138,23 +178,54 @@ EXAMPLES
         self._settings = settings
         self._jupyter_notebook_container_manager = jupyter_notebook_container_manager
 
+    def _get_container(self):
+        return self._jupyter_notebook_container_manager.get_container()
+
     def run(self):
         self._settings.raise_on_missing_file()
         self._settings.raise_on_schema_mismatch()
+
+        container = self._get_container()
 
         self.stdout("Stopping jupyter notebook...")
 
         try:
             self._jupyter_notebook_container_manager.stop()
+
+            success_message = "Jupyter Notebook service stopped"
             self.stdout(
-                "Jupyter Notebook service stopped",
+                success_message,
                 severity=StdoutSeverity.SUCCESS,
             )
-        except DockerContainerNotFoundError:
-            container_name = self._jupyter_notebook_container_manager.get_container().name
+
             self.stdout(
-                f"The Jupyter Notebook service named {container_name} is already stopped.",
+                dict(
+                    JupyterNotebookContainerServiceResponse(
+                        action="stop",
+                        status="success",
+                        message=success_message,
+                        container=container,
+                    ),
+                ),
+                stdout_type=StdoutType.MACHINE_READABLE,
+            )
+        except DockerContainerNotFoundError:
+            warning_message = f"The Jupyter Notebook service named {container.name} is already stopped."
+
+            self.stdout(
+                warning_message,
                 severity=StdoutSeverity.WARNING,
+            )
+            self.stdout(
+                dict(
+                    JupyterNotebookContainerServiceResponse(
+                        action="stop",
+                        status="already_stopped",
+                        message=warning_message,
+                        container=container,
+                    ),
+                ),
+                stdout_type=StdoutType.MACHINE_READABLE,
             )
 
 
