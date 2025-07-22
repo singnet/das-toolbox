c4da2ab774388a80ddc1a81aa7bb8570b6ead36d
rafael.levi <rafael.levi@singularitynet.io>
2025-07-22 12:10:51 -0300

    das-toolbox-181: Refactor query agent commands to enable json and yaml output

--- das-cli/src/commands/query_agent/query_agent_cli.py
+++ das-cli/src/commands/query_agent/query_agent_cli.py
@@ -1,16 +1,28 @@
 from injector import inject
 
-from commands.attention_broker.attention_broker_container_manager import AttentionBrokerManager
+from commands.attention_broker.attention_broker_container_manager import (
+    AttentionBrokerManager,
+)
 from commands.db.mongodb_container_manager import MongodbContainerManager
 from commands.db.redis_container_manager import RedisContainerManager
-from commands.query_agent.query_agent_container_manager import QueryAgentContainerManager
-from common import Command, CommandGroup, CommandOption, Settings, StdoutSeverity, StdoutType
+from commands.query_agent.query_agent_container_manager import (
+    QueryAgentContainerManager,
+)
+from common import (
+    Command,
+    CommandGroup,
+    CommandOption,
+    Settings,
+    StdoutSeverity,
+    StdoutType,
+)
 from common.decorators import ensure_container_running
 from common.docker.exceptions import (
     DockerContainerDuplicateError,
     DockerContainerNotFoundError,
     DockerError,
 )
+from .query_agent_container_service_response import QueryAgentContainerServiceResponse
 
 from .query_agent_container_service_response import QueryAgentContainerServiceResponse
 
