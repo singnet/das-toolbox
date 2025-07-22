1d6abb4eb2ead3f3ace2d08edfeec1f69074b363
rafael.levi <rafael.levi@singularitynet.io>
2025-07-22 13:41:32 -0300

    das-toolbox-181: Refactor inference agent commands to enable json and yaml output

--- das-cli/src/commands/inference_agent/inference_agent_cli.py
+++ das-cli/src/commands/inference_agent/inference_agent_cli.py
@@ -9,6 +9,7 @@ from commands.link_creation_agent.link_creation_agent_container_manager import (
 from common import Command, CommandGroup, CommandOption, Settings, StdoutSeverity, StdoutType
 from common.decorators import ensure_container_running
 from common.docker.exceptions import DockerContainerDuplicateError, DockerContainerNotFoundError
+from .inference_agent_container_service_response import InferenceAgentContainerServiceResponse
 
 from .inference_agent_container_service_response import InferenceAgentContainerServiceResponse
 
@@ -65,7 +66,6 @@ EXAMPLES
                 success_message,
                 severity=StdoutSeverity.SUCCESS,
             )
-
             self.stdout(
                 dict(
                     InferenceAgentContainerServiceResponse(
