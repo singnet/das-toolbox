fc3e47b6bc145104ad68b63231f67402ef8a94d9
levisingularity <rafael.levi@singularitynet.io>
2025-10-06 14:32:56 -0300

    Chores: create a new release version 0.5.5

--- das-cli/src/settings/config.py
+++ das-cli/src/settings/config.py
@@ -1,6 +1,6 @@
 import getpass
 
-VERSION = '0.5.4'
+VERSION = '0.5.5'
 RELEASE_NOTES_URL = "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 
 SERVICES_NETWORK_NAME = "das-services-network"
