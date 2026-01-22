02eb26b2718b193be902904c2202ab21fd09ed8a
rafael.levi <rafael.levi@singularitynet.io>
2026-01-22 17:40:58 -0300

    refactor: small refactor, wrap methods to avoid calling the same two methods everytime

--- das-cli/src/common/settings.py
+++ das-cli/src/common/settings.py
@@ -83,6 +83,10 @@ class Settings:
             raise FileNotFoundError(
                 f"Configuration file not found in {self.get_path()}. You can run the command `config set` to create a configuration file."
             )
+        
+    def validate_configuration_file(self):
+        self.raise_on_missing_file()
+        self.raise_on_schema_mismatch()
 
     def pretty(self) -> str:
         table_lines = []
