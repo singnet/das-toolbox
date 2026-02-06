7509888a948245984de8cc1b6316c940c245225f
levisingularity <rafael.levi@singularitynet.io>
2026-02-06 13:12:01 -0300

    Chores: create a new release version 1.0.4

--- das-cli/src/settings/config.py
+++ das-cli/src/settings/config.py
@@ -2,7 +2,7 @@ import getpass
 import tempfile
 from pathlib import Path
 
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 RELEASE_NOTES_URL = "https://raw.githubusercontent.com/singnet/das/master/docs/release-notes.md"
 
 SERVICES_NETWORK_NAME = "host"
@@ -40,10 +40,10 @@ DBMS_PEER_IMAGE_NAME = "trueagi/das"
 DBMS_PEER_IMAGE_VERSION = "latest-database-adapter-client"
 
 DAS_MORK_SERVER_IMAGE_NAME = "trueagi/das"
-DAS_MORK_SERVER_IMAGE_VERSION = "mork-server-1.0.0"
+DAS_MORK_SERVER_IMAGE_VERSION = "mork-server-1.0.3"
 
 DAS_MORK_LOADER_IMAGE_NAME = "trueagi/das"
-DAS_MORK_LOADER_IMAGE_VERSION = "mork-loader-1.0.1"
+DAS_MORK_LOADER_IMAGE_VERSION = "mork-loader-1.0.3"
 
-DAS_IMAGE_VERSION = "1.0.2-rc"
+DAS_IMAGE_VERSION = "1.0.3-rc"
 DAS_IMAGE_NAME = "trueagi/das"
