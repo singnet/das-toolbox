81ce99012473e4b45b768501939f25016dad9a22
rafael.levi <rafael.levi@singularitynet.io>
2026-03-30 17:42:02 -0300

    change old accordion to box component

--- das-dashboard/src/components/form_parts/AtomDB/AtomDB.jsx
+++ das-dashboard/src/components/form_parts/AtomDB/AtomDB.jsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionSummary, AccordionDetails, Typography, TextField, MenuItem, Button } from "@mui/material"
+import { Box, Typography, TextField, MenuItem } from "@mui/material"
 import { RedisMongoOptions } from "./RedisMongo"
 import { MorkMongoOptions } from "./MorkMongo"
 import { useState } from "react"
@@ -9,37 +9,57 @@ export default function AtomDBForm({ onSectionSave }) {
   const [type, setType] = useState("")
 
   return (
-    <Accordion defaultExpanded>
-      <AccordionSummary><Typography>AtomDB Configuration</Typography></AccordionSummary>
-      <AccordionDetails>
-        <TextField select fullWidth label="Database Type" value={type} onChange={(e) => setType(e.target.value)}>
-          <MenuItem value="redismongodb">Redis + MongoDB</MenuItem>
-          <MenuItem value="morkmongodb">Mork + MongoDB</MenuItem>
-          <MenuItem value="inmemorydb">In Memory</MenuItem>
-          <MenuItem value="remotedb">Remote DB (Multi-Peer)</MenuItem>
-        </TextField>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      
+      <Typography variant="h6">
+        AtomDB Configuration
+      </Typography>
 
-        {
-        type === "redismongodb" && (
-          <RedisMongoOptions onSave={(data) => onSectionSave("atomdb", { type, ...data })} />
-        )
-        }
-        {
-          type === "morkmongodb" && (
-            <MorkMongoOptions onSave={(data) => onSectionSave("atomdb", { type, ...data})}/>
-          )
-        }
-        {
-          type === "inmemorydb" && (
-            <InMemoryOptions onSave={(data) => onSectionSave("atomdb", { type, ...data})}/>
-          )
-        }
-        {
-          type === "remotedb" && (
-            <RemoteDBOptions onSave={(data) => onSectionSave("atomdb", { type, ...data })} />
-          )
-        }
-      </AccordionDetails>
-    </Accordion>
+      <TextField
+        select
+        fullWidth
+        label="Database Type"
+        value={type}
+        onChange={(e) => setType(e.target.value)}
+      >
+        <MenuItem value="redismongodb">Redis + MongoDB</MenuItem>
+        <MenuItem value="morkmongodb">Mork + MongoDB</MenuItem>
+        <MenuItem value="inmemorydb">In Memory</MenuItem>
+        <MenuItem value="remotedb">Remote DB (Multi-Peer)</MenuItem>
+      </TextField>
+
+      {type === "redismongodb" && (
+        <RedisMongoOptions
+          onSave={(data) =>
+            onSectionSave("atomdb", { type, ...data })
+          }
+        />
+      )}
+
+      {type === "morkmongodb" && (
+        <MorkMongoOptions
+          onSave={(data) =>
+            onSectionSave("atomdb", { type, ...data })
+          }
+        />
+      )}
+
+      {type === "inmemorydb" && (
+        <InMemoryOptions
+          onSave={(data) =>
+            onSectionSave("atomdb", { type, ...data })
+          }
+        />
+      )}
+
+      {type === "remotedb" && (
+        <RemoteDBOptions
+          onSave={(data) =>
+            onSectionSave("atomdb", { type, ...data })
+          }
+        />
+      )}
+
+    </Box>
   )
 }
