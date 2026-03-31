990915227d0eafe324ff7f27ef1970195a854203
rafael.levi <rafael.levi@singularitynet.io>
2026-03-31 08:55:35 -0300

    brokers section

---
+++ das-dashboard/src/components/form_parts/Brokers/Brokers.jsx
@@ -0,0 +1,82 @@
+import { Box, Button, TextField, Typography } from "@mui/material"
+import { useRef } from "react"
+
+export function BrokersForm({ onSectionSave }) {
+  const form = useRef({
+    attentionPort: "40001",
+
+    contextPort: "40006",
+    contextRange: "46000:46999",
+
+    atomdbPort: "40007",
+    atomdbRange: "47000:47999"
+  })
+
+  const isValidRange = (range) => {
+    const match = range.match(/^(\d+):(\d+)$/)
+    if (!match) return false
+
+    const start = Number(match[1])
+    const end = Number(match[2])
+
+    return start < end && start >= 1 && end <= 65535
+  }
+
+  const handleSave = () => {
+    const data = form.current
+
+    const section = {
+      attention: {
+        endpoint: `localhost:${data.attentionPort}`
+      },
+      context: {
+        endpoint: `localhost:${data.contextPort}`,
+        ports_range: data.contextRange
+      },
+      atomdb: {
+        endpoint: `localhost:${data.atomdbPort}`,
+        ports_range: data.atomdbRange
+      }
+    }
+
+    onSectionSave("brokers", section)
+  }
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <Typography variant="h6">Brokers Configuration</Typography>
+
+      <TextField
+        label="Attention Broker Port"
+        type="number"
+        onChange={(e) => (form.current.attentionPort = e.target.value)}
+      />
+
+      <TextField
+        label="Context Broker Port"
+        type="number"
+        onChange={(e) => (form.current.contextPort = e.target.value)}
+      />
+      <TextField
+        label="Context Broker Range"
+        helperText={"Port range must be in 'start:end' format"}
+        onChange={(e) => (form.current.contextRange = e.target.value)}
+      />
+
+      <TextField
+        label="AtomDB Broker Port"
+        type="number"
+        onChange={(e) => (form.current.atomdbPort = e.target.value)}
+      />
+      <TextField
+        label="AtomDB Broker Range"
+        helperText={"Port range must be in 'start:end' format"}
+        onChange={(e) => (form.current.atomdbRange = e.target.value)}
+      />
+
+      <Button variant="contained" color="success" onClick={handleSave}>
+        Save brokers section
+      </Button>
+    </Box>
+  )
+}
