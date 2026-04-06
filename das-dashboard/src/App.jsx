56f5c6df03f8009210006e0020eb09a6b2a5cec9
rafael.levi <rafael.levi@singularitynet.io>
2026-04-06 19:01:39 -0300

    Renamed main page to setupdaspage and added global providers

--- das-dashboard/src/App.jsx
+++ das-dashboard/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
-import MainPage from './pages/main/MainPage.jsx'
+import SetupDasPage from './pages/setup_das/SetupDas.jsx'
 import './App.css'
+import { ToastProvider } from './components/global_components/ToastProvider.jsx'
+import { ConfigurationProvider } from './components/global_components/ConfigurationProvider.jsx'
 
 function App() {
 
   return (
     <>
-      <MainPage></MainPage>
+      <ToastProvider>
+        <ConfigurationProvider>
+          <SetupDasPage></SetupDasPage>
+        </ConfigurationProvider>
+      </ToastProvider>
     </>
   )
   
