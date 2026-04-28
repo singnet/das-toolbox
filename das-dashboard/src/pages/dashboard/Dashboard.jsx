56ac332a77318bad671c549b84ea86ed50e2df49
rafael.levi <rafael.levi@singularitynet.io>
2026-04-28 18:33:23 -0300

    fix imports

--- das-dashboard/src/pages/dashboard/Dashboard.jsx
+++ das-dashboard/src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
-import { CPUViewChart } from "../../components/dashboard/CPUViewChart";
-import { MainContent } from "../../components/dashboard/MainContent";
-import { MemoryViewChart } from "../../components/dashboard/MemoryViewChart";
-import { ServerTab } from "../../components/dashboard/ServerTab";
-import { SideBar } from "../../components/dashboard/SideBar";
-import "./Dashboard.css"
+import { CPUViewChart } from "../../components/dashboard/MainContent/CPUViewChart";
+import { MainContent } from "../../components/dashboard/MainContent/MainContent";
+import { MemoryViewChart } from "../../components/dashboard/MainContent/MemoryViewChart";
+import { ServerTab } from "../../components/dashboard/MainContent/servertab/ServerTab";
+import { SideBar } from "../../components/dashboard/MainContent/SideBar";
 import ArchitectureView from "../../components/dashboard/ArchitectureView/ArchitectureViewCard";
+
 import { useDashboardContext } from "../../components/global_providers/DashboardContextProvider";
+
+import "./Dashboard.css"
 
 export default function DashboardPage() {
   const { currentContext, setCurrentContext } = useDashboardContext()
