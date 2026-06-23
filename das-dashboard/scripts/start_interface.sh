5886ee28fdf49c269c41f65e0cfd097df67198e6
rafael.levi <rafael.levi@singularitynet.io>
2026-06-23 12:47:24 -0300

    fix container naiming mistake on startup script.

--- das-dashboard/scripts/start_interface.sh
+++ das-dashboard/scripts/start_interface.sh
@@ -22,7 +22,7 @@ echo "Starting backend on port ${BACK_PORT}..."
 docker rm -f ui_backend >/dev/null 2>&1 || true
 
 docker run -d \
-  --name web-interface-frontend \
+  --name web-interface-backend \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/das-cli:/usr/bin/das-cli:ro \
   -v /opt/web-das:/opt/web-das \
@@ -37,7 +37,7 @@ echo "Starting frontend on port ${FRONT_PORT}..."
 docker rm -f das-dashboard >/dev/null 2>&1 || true
 
 docker run -d \
-  --name web-interface-backend \
+  --name web-interface-frontend \
   --network host \
   das-dashboard:latest \
   npm run dev -- --host 0.0.0.0 --port ${FRONT_PORT}
