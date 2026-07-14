ce841e313a00b9f06b71ab03f33b16b584b11493
rafael.levi <rafael.levi@singularitynet.io>
2026-07-14 18:04:32 -0300

    Added query page to routing and navbar

--- das-dashboard/src/App.jsx
+++ das-dashboard/src/App.jsx
@@ -5,11 +5,12 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 
 import { Box } from "@mui/material"
-import Navbar from "./components/top_nav_bar/NavBar.jsx"
+import NavBar from "./components/top_nav_bar/NavBar.jsx"
 import SetupDasPage from './pages/setup_das/SetupDas.jsx'
 import DashboardPage from './pages/dashboard/Dashboard.jsx'
 import DashboardContextProvider from './components/global_providers/DashboardContextProvider.jsx'
 import ProfilePage from './pages/profile/ProfilePage.jsx'
+import QueryPage from './pages/query/QueryPage.jsx'
 import { DialogProvider } from './components/global_providers/DialogProvider.jsx'
 
 
@@ -18,17 +19,18 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <DashboardContextProvider>
         <Box sx={{ display: "flex", flexDirection: "column", height: "100vh", overflow: "hidden" }}>
-          <Navbar />
+          <NavBar />
           <Box component="main" sx={{ flex: 1, minHeight: 0, overflow: "hidden" }}>
             <Routes>
                   <Route path='/configuration' element={
                     <DialogProvider>
                       <ToastProvider>
-                        <ConfigurationProvider>
-                          <SetupDasPage/>
-                        </ConfigurationProvider>
+                        <DashboardContextProvider>
+                          <ConfigurationProvider>
+                            <SetupDasPage/>
+                          </ConfigurationProvider>
+                        </DashboardContextProvider>
                       </ToastProvider>
                     </DialogProvider>
                   }/>
@@ -42,14 +44,20 @@ function App() {
                   <Route path='/dashboard' element={
                     <DialogProvider>
                       <ToastProvider>
-                          <DashboardPage />
+                        <DashboardPage />
+                      </ToastProvider>
+                    </DialogProvider>
+                  }/>
+                  <Route path='/query' element={
+                    <DialogProvider>
+                      <ToastProvider>
+                        <QueryPage />
                       </ToastProvider>
                     </DialogProvider>
                   }/>
             </Routes>
           </Box>
         </Box>
-        </DashboardContextProvider>
       </BrowserRouter>
     </>
   )
--- das-dashboard/src/components/top_nav_bar/NavBar.jsx
+++ das-dashboard/src/components/top_nav_bar/NavBar.jsx
@@ -30,7 +30,7 @@ const AppBarItem = styled("a")({
   },
 });
 
-export default function Navbar() {
+export default function NavBar() {
   return (
     <MainAppBar position="static" elevation={0}>
       <StyledToolbar>
@@ -43,6 +43,7 @@ export default function Navbar() {
           <AppBarItem href="/profiles">Profile</AppBarItem>
           <AppBarItem href="/configuration">Configuration</AppBarItem>
           <AppBarItem href="/dashboard">Dashboard</AppBarItem>
+          <AppBarItem href="/query">Query</AppBarItem>
         </AppBarItems>
 
       </StyledToolbar>
